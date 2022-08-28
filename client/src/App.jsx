bf23b24c0700a538502db3c54a7a9b9015e986c7
nandweza <allannandweza@gmail.com>
2022-08-28 13:33:35 +0300

    created navbar components

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import Home from "./home/Home";
+
+const App = () => {
+  return <Home />
+}
+
+export default App;
---
+++ client/src/home/Home.jsx
@@ -0,0 +1,14 @@
+//import { AcUnit } from "@material-ui/icons"
+import Navbar from "../components/navbar/Navbar"
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Navbar/>
+      <img src="https://avatars.githubusercontent.com/u/63627595?v=4" alt="" width="100%" />
+    </div>
+  )
+}
+
+export default Home
